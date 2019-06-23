90678b3957286a421c165ff026d3853e0e206dd1
KeyVote <52130637+KeyVote@users.noreply.github.com>
2019-06-23 20:53:49 +0200

    The voting system and the p5js code.

--- Main/voting system code/sketch.js
+++ Main/voting system code/sketch.js
@@ -1,23 +1,26 @@
-//Make a object for every party so they have votes
+function setup() {
+	noCanvas();
+}
 
-function Party(name, ideology, votes) {
+function Party(name, ideology, votes, partyLeader) {
 		this.name = name;
 		this.ideology = ideology;
 		this.votes = votes;
-	}
+		this.partyLeader = partyLeader;
+}
 
-let fascistParty = new Party("The fascist party", "Fascism", 0)
-let communistParty = new Party("The communist party", "Communism", 0)
-let democraticParty = new Party("The democratic party", "Center-left", 0)
-let republicanParty = new Party("The republican party", "Center-right", 0)
+let fascistParty = new Party("The fascist party", "Fascism", 0, "Julius Di Vanci");
+let communistParty = new Party("The communist party", "Communism", 0, "Vladimir Trotsky");
+let democraticParty = new Party("The democratic party", "Liberalism", 0, "Anna Devago");
+let republicanParty = new Party("The republican party", "Consvervatism", 0, "Luis Lint");
 
 function voting() {
-	let parties = [fascistParty, communistParty, democraticParty, republicanParty]
-	let winningParty
+	let parties = [fascistParty, communistParty, democraticParty, republicanParty];
+	let winningParty;
 
 	for (const party of parties) {
 		party.votes = Math.floor((Math.random() * 1000) + 1)
-		console.log(party.votes)
+		console.log(party.votes);
 	}
 
 	let totalVotes = 0
@@ -26,7 +29,6 @@ function voting() {
 	}
 
 	let onePercent = 100 / totalVotes
-
 	const mostVotes = Math.max(fascistParty.votes, communistParty.votes, democraticParty.votes, republicanParty.votes)
 
 	for (const party of parties) {
@@ -35,14 +37,22 @@ function voting() {
 		}
 	}
 
-	console.log("There were a total of ".concat(totalVotes, " votes!"))
+	console.log("There were a total of ".concat(totalVotes, " votes!"));
 
 	for (const party of parties) {
 		let partyPercentage = onePercent * party.votes
-		console.log(party.name.concat(" won ", partyPercentage.toFixed(2), " percent of the vote!"))
+		console.log(party.name.concat(" won ", partyPercentage.toFixed(2), " percent of the vote!"));
 	}
+	console.log(winningParty.name.concat(" got ", winningParty.votes, " votes and has won the election!"));
 
-	console.log(winningParty.name.concat(" got ", winningParty.votes, " votes and has won the election!"))
+	function showText(id, text) {
+		input = document.getElementById(id);
+		input.innerText = text
+	}
+	showText("WinningParty", "The party that won: ".concat(winningParty.name, "!"))
+	showText("WinningPercentage", "The winning party's percentage of the vote: ".concat((onePercent * winningParty.votes).toFixed(2), "%"))
+	showText("WinningIdeology", "The winning party's ideology: ".concat(winningParty.ideology, "!"))
+	showText("WinningPartyLeader", "The winning party's leader: ".concat(winningParty.partyLeader, "!"))
 }
 
 voting()
