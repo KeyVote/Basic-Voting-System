b7451c9eb42d8cc35cd325af643828cf2d786062
KeyVote <52130637+KeyVote@users.noreply.github.com>
2019-06-25 21:31:10 +0200

    An early population system

--- Main/voting system code/sketch.js
+++ Main/voting system code/sketch.js
@@ -1,18 +1,21 @@
 function setup() {
-	noCanvas();
+	noCanvas()
 }
 
+console.log(totalPOPs);
+console.log(popsArray);
+
 function showText(id, text) {
-		input = document.getElementById(id);
-		input.innerText = text
-	}
+	input = document.getElementById(id);
+	input.innerText = text
+}
 
 function Party(name, ideology, votes, pFirstName, pLastName) {
-		this.name = name;
-		this.ideology = ideology;
-		this.votes = votes;
-		this.pFirstName = pFirstName;
-		this.pLastName = pLastName;
+	this.name = name;
+	this.ideology = ideology;
+	this.votes = votes;
+	this.pFirstName = pFirstName;
+	this.pLastName = pLastName;
 }
 
 let firstNames = ["Antonio", "James", "Harald", "Luis", "Nicholas", "Marlyn", "Jessica", "Jonas", "Mary",
@@ -25,28 +28,31 @@ let lastNames = [" Van Holland", " O'Brien", " Andersson", " Di Clara", " Smith"
 " Kolar", " García", " González", " Keller", " Kaya", " White", " Ozols", " Hoti", " De Luca",
 " Murphy", " Schmidt", " Kapanadze", " Dubois", " Nyman", " Mäkelä", " Petersen", " Saar", " Kloö"];
 
+
 let fascistParty = new Party("The fascist party", "Fascism", 0, firstNames[Math.floor((Math.random() * 32) + 1)], lastNames[Math.floor((Math.random() * 32) + 1)]);
 let communistParty = new Party("The communist party", "Communism", 0, firstNames[Math.floor((Math.random() * 32) + 1)], lastNames[Math.floor((Math.random() * 32) + 1)]);
-let democraticParty = new Party("The democratic party", "Liberalism", 0, firstNames[Math.floor((Math.random() * 32) + 1)], lastNames[Math.floor((Math.random() * 32) + 1)]);
-let republicanParty = new Party("The republican party", "Consvervatism", 0, firstNames[Math.floor((Math.random() * 32) + 1)], lastNames[Math.floor((Math.random() * 32) + 1)]);
-
-let parties = [fascistParty, communistParty, democraticParty, republicanParty];
+let liberalParty = new Party("The liberal party", "Liberalism", 0, firstNames[Math.floor((Math.random() * 32) + 1)], lastNames[Math.floor((Math.random() * 32) + 1)]);
+let conservativeParty = new Party("The conservative party", "Consvervatism", 0, firstNames[Math.floor((Math.random() * 32) + 1)], lastNames[Math.floor((Math.random() * 32) + 1)]);
+let parties = [fascistParty, communistParty, liberalParty, conservativeParty];
 
 function voting() {
 	let winningParty;
+	let totalVotes = 0;
 
-	for (const party of parties) {
-		party.votes = Math.floor((Math.random() * 1000) + 1)
-		console.log(party.votes);
-	}
+	//for (const party of parties) {
+		//party.votes = Math.floor((Math.random() * 1000) + 1)
+	//}
+	fascistParty.votes = fascistPOPs;
+	communistParty.votes = communistPOPs;
+	liberalParty.votes = liberalPOPs;
+	conservativeParty.votes = conservativePOPs;
 
-	let totalVotes = 0
 	for (const party of parties) {
 		totalVotes = totalVotes + party.votes
 	}
 
 	let onePercent = 100 / totalVotes
-	const mostVotes = Math.max(fascistParty.votes, communistParty.votes, democraticParty.votes, republicanParty.votes)
+	const mostVotes = Math.max(fascistParty.votes, communistParty.votes, liberalParty.votes, conservativeParty.votes)
 
 	for (const party of parties) {
 		if (party.votes == mostVotes) {
@@ -63,10 +69,11 @@ function voting() {
 
 	console.log(winningParty.name.concat(" got ", winningParty.votes, " votes and has won the election!"));
 
-	showText("WinningParty", "The party that won: ".concat(winningParty.name, "!"))
-	showText("WinningPercentage", "The winning party's percentage of the vote: ".concat((onePercent * winningParty.votes).toFixed(2), "%"))
-	showText("WinningIdeology", "The winning party's ideology: ".concat(winningParty.ideology, "!"))
-	showText("WinningPartyLeader", "The winning party's leader: ".concat(winningParty.pFirstName,  winningParty.pLastName, "!"))
+	showText("WinningParty", "The party that won: ".concat(winningParty.name, "!"));
+	showText("WinningPercentage", "The winning party's percentage of the vote: ".concat((onePercent * winningParty.votes).toFixed(2), "%"));
+	showText("WinningIdeology", "The winning party's ideology: ".concat(winningParty.ideology, "!"));
+	showText("WinningPartyLeader", "The winning party's leader: ".concat(winningParty.pFirstName,  winningParty.pLastName, "!"));
+	showText("totalPopulation", "The total population is: ".concat(totalPOPs, " people!"));
 }
 
 voting()
